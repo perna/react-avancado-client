f4010d4d5944880ea4ec57f1544c9441bdddffae
Anderson Meira <anderson.meira@gmail.com>
2020-11-17 18:30:35 -0300

    feat(boilerplate): create new main page

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,14 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="React Avançado" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="Um desenvolvedor de frente para uma tela com código" />
+  </S.Wrapper>
 )
 
 export default Main
